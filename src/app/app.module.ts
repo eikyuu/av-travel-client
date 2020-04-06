29d31cb2f835e5f69ce51189f02fc6a5df9be79c
Eikyuu <v.duguet@laposte.net>
2020-04-06 21:25:21 +0200

    searchbar css and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    BannerComponent
+    BannerComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
